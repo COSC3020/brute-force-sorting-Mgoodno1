a3138346937f1480eab0752af9455976f9774653
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-12-10 01:37:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
 function permutationSort(a) {
-    return 0;
+    let count = 0;
+
+    function isSorted(arr) {
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function permute(arr, startIndex) {
+        if (startIndex === arr.length) {
+            count++;
+
+            if (isSorted(arr)) {
+                return true;
+            }
+            else{
+                return false;
+
+            }
+        }
+        
+        for (let i = startIndex; i < arr.length; i++) {
+            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]];
+            if (permute(arr, startIndex + 1)) {
+                return true;
+            }
+            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]];
+        }
+        return false;
+    }
+
+    permute(a, 0);
+    return count;
 }
